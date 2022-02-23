30329ca399d4e9589b803732fa99e8aeda352bb6
Laurentiu Onac <onac.laurentiu@gmail.com>
2022-02-23 11:40:12 +0200

    [#3] Added all possible dependency types for future implementation.

--- buildSrc/src/main/kotlin/dependencies/extensions/DependencyHandlerExtensions.kt
+++ buildSrc/src/main/kotlin/dependencies/extensions/DependencyHandlerExtensions.kt
@@ -4,15 +4,30 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 import dependencies.TestDependencies
 import dependencies.AndroidTestDependencies
 
-private const val TEST_IMPLEMENTATION = "testImplementation"
-private const val ANDROID_TEST_IMPLEMENTATION = "androidTestImplementation"
+private const val TYPE_TEST_IMPLEMENTATION = "testImplementation"
+private const val TYPE_ANDROID_TEST_IMPLEMENTATION = "androidTestImplementation"
+private const val TYPE_API = "api"
+private const val TYPE_IMPLEMENTATION = "implementation"
+private const val TYPE_KAPT = "kapt"
 
 fun DependencyHandler.testImplementation(dependencyName: String) {
-    add(TEST_IMPLEMENTATION, dependencyName)
+    add(TYPE_TEST_IMPLEMENTATION, dependencyName)
 }
 
 fun DependencyHandler.androidTestImplementation(dependencyName: String) {
-    add(ANDROID_TEST_IMPLEMENTATION, dependencyName)
+    add(TYPE_ANDROID_TEST_IMPLEMENTATION, dependencyName)
+}
+
+fun DependencyHandler.api(dependencyName: String) {
+    add(TYPE_API, dependencyName)
+}
+
+fun DependencyHandler.implementation(dependencyName: String) {
+    add(TYPE_IMPLEMENTATION, dependencyName)
+}
+
+fun DependencyHandler.kapt(dependencyName: String) {
+    add(TYPE_KAPT, dependencyName)
 }
 
 fun DependencyHandler.addAndroidAndTestDependencies() {
