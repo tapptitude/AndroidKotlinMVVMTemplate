2e2c7e3e0d1ccb1e37d138b6125945b943d2adda
Andrei Teslovan <andrei.teslovan@tapptitude.com>
2022-11-11 19:38:49 +0200

    Document the isNonStable function

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,6 +63,17 @@ versionCatalogUpdate {
     }
 }
 
+/**
+ * Identifies if the dependecy version is stable or not by looking if the version contains
+ * any of the RELEASE, FINAL or GA keywords or if the version has the following format:
+ * - 1 or more of any char in the range "0" to "9" or ",", ".", "v", "-" characters
+ *
+ * followed by
+ *
+ * - at most 1 time any of the "-", "r" charachers
+ * @param version The version of the dependency.
+ * @return True if the dependecy version is not stable, false otherwise.
+ */
 fun isNonStable(version: String): Boolean {
     val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
