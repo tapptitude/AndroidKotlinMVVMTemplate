a878f178b92aa21b66d197f93bc3de9d9cc3bbb8
Andrei Teslovan <ateslovan@pm.me>
2023-04-11 10:36:10 +0300

    Add Compose lint checks (#30)
    * Add Compose lint checks
    
    * Fix lint warning regarding modifier
    
    ---------
    
    Co-authored-by: Andrei Teslovan <andrei.teslovan@tapptitude.com>

--- buildSrc/src/main/kotlin/LibraryComposeConventionPlugin.kt
+++ buildSrc/src/main/kotlin/LibraryComposeConventionPlugin.kt
@@ -2,7 +2,10 @@ import com.android.build.api.dsl.LibraryExtension
 import ext.addAndroidComposeConfig
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 
 class LibraryComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -10,6 +13,12 @@ class LibraryComposeConventionPlugin : Plugin<Project> {
 
         target.extensions.configure<LibraryExtension> {
             target.addAndroidComposeConfig(this)
+        }
+
+        val libs = target.extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+        target.dependencies {
+            add("lintChecks", libs.findLibrary("compose.lintChecks").get())
         }
     }
 }
